7abb410c51f594266949eb0ef1f95a7e5f2ac197
Mansi-298 <waghmansi298@gmail.com>
2025-08-02 10:34:42 +0530

    added

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://vercel-frontend-silk.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Import routes
